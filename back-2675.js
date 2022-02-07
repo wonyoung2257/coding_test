a38ee4c613ed5baf6949be07ca40c8563c2ff0d2
Won-young <dnjsdud2257@gmail.com>
2022-02-07 10:48:44 +0900

    백준 2675

--- back-2675.js
+++ back-2675.js
@@ -1,12 +1,21 @@
 const fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().split("\r\n");
-input;
+let input = fs.readFileSync("input.txt").toString().trim().split("\r\n");
+//문자열을 자르고 뒤에 공백에 주의
 
 soultion(input);
+
 function soultion(input) {
-  let obj = {};
   for (let i = 1; i < input.length; i++) {
-    let arr = input[i].split(" ");
-    arr
+    let answer = [];
+    let [num, word] = input[i].split(" ");
+    console.log(typeof word);
+    let temp = word.split("");
+    console.log(temp);
+    temp.map((el) => {
+      for (let j = 0; j < +num; j++) {
+        answer.push(el);
+      }
+    });
+    console.log(answer.join(""));
   }
 }
